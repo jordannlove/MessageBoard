109e6bec106dc668ccdf60c0296b5071accdc540
Jordan Love <jnlove314@gmail.com>
2019-04-04 09:12:25 -0500

    Sort out confusion between CreatePosts and CreateUser

--- CreatePosts.php
+++ CreatePosts.php
@@ -5,6 +5,7 @@
 //access the global array called $_POST to get the values from the text fields
 //
 $username = $_POST["name"];
+$blogPost = $_POST["blog"];
 
 $mysqli = new mysqli("mysql.eecs.ku.edu", "j863l378", "eithai3k", "j863l378");
 
@@ -37,35 +38,36 @@ if($result = $mysqli->query($query))
       $taken = true;
     }
   }
-  //if username field is empty
-  if($username == "")
+  //if post is empty
+  if($blogPost == "")
   {
-    printf("Must add a username.");
+    printf("Text box must not be empty.");
   }
   //if info is valid, store in database
-  else if ($taken == false)
+  else if ($taken == true)
   {
-    $q1 = "INSERT INTO Users (user_id) VALUES ('$username')";
+    $q1 = "INSERT INTO Posts (post_id, content, author_id) VALUES ('NULL', '$blogPost', '$username')";
     //query to check if actually stored
-    if($mysqli->query($q1) === false )
+    if($mysqli->query($q1) == true )
     {
-      echo "Uh-oh, there was an error in making your account.";
+      echo "Posted! <br>";
     }
     else
     {
-      echo "Account created!<br>";
+      echo "Uh-oh, there was an error in publishing your post.";
     }
   }
   //if $taken is true, then username is taken
   else
   {
-    printf("That username is already in use.");
+    printf("That username is not valid.");
   }
   //free result set
   $result->free();
 }
 
 echo "<br>Username: " . $username . "<br>";
+echo "Post: " . $blogPost . "<br>";
 
 //
 // close connection
