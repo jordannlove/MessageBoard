864209f238d581786f72bd93d41ff0d0a2886cb9
Jordan Love <jnlove314@gmail.com>
2019-04-03 22:51:44 -0500

    add mysqli to check connection

--- CreateUser.php
+++ CreateUser.php
@@ -1,10 +1,26 @@
 
 <?php
+&mysqli = new mysqli("database_URL", "j863l378", "ethai3k", "j863l378");
+
+//
+// check connection
+//
+if ($mysqli->connect_errno)
+{
+  printf("Connect failed: %s\n", $mysqli->connect_error);
+  exit();
+}
+
 //access the global array called $_POST to get the values from the text fields
 $name = $_POST["name"];
 $email = $_POST["email"];
 
 echo "Name: " . $name . "<br>";
 echo "Email: " . $email . "<br>";
+
+//
+// close connection
+//
+$mysqli->close();
 
 ?>
