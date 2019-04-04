9408c94e385f6e97ce4d05ea6bcef27372f857b0
Jordan Love <jnlove314@gmail.com>
2019-04-04 00:44:15 -0500

    Add database functionality

--- CreateUser.php
+++ CreateUser.php
@@ -1,6 +1,13 @@
 
 <?php
-&mysqli = new mysqli("database_URL", "j863l378", "ethai3k", "j863l378");
+
+//
+//access the global array called $_POST to get the values from the text fields
+//
+$username = $_POST["name"];
+$blogPost = $_POST["blog"];
+
+$mysqli = new mysqli("mysql.eecs.ku.edu", "j863l378", "eithai3k", "j863l378");
 
 //
 // check connection
@@ -11,12 +18,56 @@ if ($mysqli->connect_errno)
   exit();
 }
 
-//access the global array called $_POST to get the values from the text fields
-$name = $_POST["name"];
-$email = $_POST["email"];
+//
+// obtains user_id from database
+//
+$query = "SELECT user_id FROM Users";
 
-echo "Name: " . $name . "<br>";
-echo "Email: " . $email . "<br>";
+//
+// $result is object containing rows of info
+//
+if($result = $mysqli->query($query))
+{
+  $taken = false;
+  //fetch associative array
+  while($row = $result->fetch_assoc())
+  {
+    //if username exists
+    if($row["user_id"] == $username)
+    {
+      $taken = true;
+    }
+  }
+  //if post is empty
+  if($blogPost == "")
+  {
+    printf("Text box must not be empty.");
+  }
+  //if info is valid, store in database
+  else if ($taken == false)
+  {
+    $q1 = "INSERT INTO Posts (post_id, content, author_id) VALUES ('NULL', '$blogPost', '$username')";
+    //query to check if actually stored
+    if($mysqli->query($q1) === false)
+    {
+      echo "Uh-oh, there was an error in publishing your post.";
+    }
+    else
+    {
+      echo "Posted! <br>";
+    }
+  }
+  //if $taken is true, then username is taken
+  else
+  {
+    printf("That username is already in use.");
+  }
+  //free result set
+  $result->free();
+}
+
+echo "Username: " . $username . "<br>";
+echo "Post: " . $blogPost . "<br>";
 
 //
 // close connection
